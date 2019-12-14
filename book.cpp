b9e75301400add778e703ad67ac95e9bb668a446
Julia0212 <yulia.samartseva@gmail.com>
2019-12-14 19:38:49 +0200

    One author book, many author book implementation

--- book.cpp
+++ book.cpp
@@ -1,5 +1,13 @@
 #include "book.h"
 
+Book::Book(std::string name, time_t publicationDate, std::string genre, int pageNumber)
+{
+    this->name = name;
+    this->publicationDate = publicationDate;
+    this->genre = genre;
+    this->pageNumber = pageNumber;
+}
+
 std::string Book::getName() const
 {
     return name;
@@ -40,9 +48,9 @@ void Book::setPageNumber(int value)
     pageNumber = value;
 }
 
-Book::Book()
+oneAuthorBook::oneAuthorBook(Author author, std::string name, time_t publicationDate, std::string genre, int pageNumber)
+    : Book(name, publicationDate, genre, pageNumber), author(author)
 {
-
 }
 
 Author oneAuthorBook::getAuthor() const
@@ -53,4 +61,20 @@ Author oneAuthorBook::getAuthor() const
 void oneAuthorBook::setAuthor(const Author &value)
 {
     author = value;
+}
+
+
+manyAuthorBook::manyAuthorBook(std::vector<Author> authors, std::string name, time_t publicationDate, std::string genre, int pageNumber)
+: Book(name, publicationDate, genre, pageNumber), authors(authors)
+{
+}
+
+std::vector<Author> manyAuthorBook::getAuthors() const
+{
+    return authors;
+}
+
+void manyAuthorBook::setAuthors(const std::vector<Author> &value)
+{
+    authors = value;
 }
--- book.h
+++ book.h
@@ -1,19 +1,20 @@
 #ifndef BOOK_H
 #define BOOK_H
 #include <string>
+#include <unordered_map>
+#include <vector>
 #include <ctime>
 #include <author.h>
 
 class Book
 {
 private:
-    std::string name;
+    std::string name = "";
     time_t publicationDate;
-    std::string genre;
-    int pageNumber;
-
+    std::string genre = "";
+    int pageNumber = 0;
 public:
-    Book();
+    Book(std::string name, time_t publicationDate, std::string genre, int pageNumber);
     std::string getName() const;
     void setName(const std::string &value);
     time_t getPublicationDate() const;
@@ -29,8 +30,27 @@ class oneAuthorBook : public Book
 private:
     Author author;
 public:
+    oneAuthorBook(Author author, std::string name, time_t publicationDate, std::string genre, int pageNumber);
     Author getAuthor() const;
     void setAuthor(const Author &value);
+};
+
+class manyAuthorBook : public Book
+{
+private:
+    std::vector<Author> authors;
+public:
+    manyAuthorBook(std::vector<Author> authors, std::string name, time_t publicationDate, std::string genre, int pageNumber);
+    std::vector<Author> getAuthors() const;
+    void setAuthors(const std::vector<Author> &value);
+};
+
+class manyAuthorBook_bookParts : public manyAuthorBook
+{
+private:
+    std::unordered_map<Author, int> bookPercentDone;
+public:
+    manyAuthorBook_bookParts(std::unordered_map<Author, int> bookPercentDone, std::string name, time_t publicationDate, std::string genre, int pageNumber);
 };
 
 #endif // BOOK_H
