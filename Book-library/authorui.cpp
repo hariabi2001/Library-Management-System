32e93aa4d5f2265c812c2477c5a0918a77486051
hariabi2001 <61871536+hariabi2001@users.noreply.github.com>
2023-10-13 01:43:56 +0530

    Update authorui.cpp

--- Book-library/authorui.cpp
+++ Book-library/authorui.cpp
@@ -7,105 +7,68 @@
 #include "algorithms.h"
 #include "map"
 #include "set"
-authorUI::authorUI(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::authorUI)
-{
-    ui->setupUi(this);
-    addData();
-}
-authorUI::~authorUI()
-{
-    
-}
-void authorUI::addToList(Author *author)
-{
+
+// Helper function to add an item to authorsTable
+void authorUI::addItemToList(const QString &text) {
     auto item = std::make_unique<QListWidgetItem>();
-    item->setText(QString::fromStdString(author->getRealName()));
+    item->setText(text);
     ui->authorsTable->addItem(item);
 }
-void authorUI::addStringToList(std::string value)
-{
-    auto item = std::make_unique<QListWidgetItem>();
-    item->setText(QString::fromStdString(value));
-    ui->authorsTable->addItem(item);
-}
-void authorUI::addData()
-{
-    auto author1 = std::make_shared<Author>("Cris Smith", "Classic");
-    auto author2 = std::make_shared<Author>("Albert Tuna", "History");
-    auto author3 = std::make_shared<Author>("Nickolas Tame", "History");
-    auto author4 = std::make_shared<Author>("Khristina Palson", "Thriller");
-    auto author5 = std::make_shared<Author>("John Kenner", "Science Fiction");
-    authors.push_back(author1);
-    authors.push_back(author2);
-    authors.push_back(author3);
-    authors.push_back(author4);
-    authors.push_back(author5);
-    auto data1 = std::make_shared<oneAuthor>(author4, "Kelly");
-    std::vector<author_pseudonym> many1;
-    many1.push_back(author_pseudonym(author1, "Derek"));
-    many1.push_back(author_pseudonym(author2, "Felly"));
-    many1.push_back(author_pseudonym(author3, "Dams"));
-    many1.push_back(author_pseudonym(author5, "Parkinson"));
-    auto data2 = std::make_shared<manyAuthors>(many1);
-    algorithms algorithm;
-    Book *first = new Book("Garry", algorithm.getTime("Sun Feb 13 00:26:36 2000"), "Classic", 123, data1);
-    Book *second = new Book("July moon", algorithm.getTime("Mon Feb 7 00:21:00 2005"), "Classic", 123, data2);
-    books.push_back(first);
-    books.push_back(second);
-    addToList(author1);
-    addToList(author2);
-    addToList(author3);
-    addToList(author4);
-    addToList(author5);
-}
-void authorUI::on_pageSort_clicked()
-{
-    struct Cmp
-    {
-        bool operator ()(const std::pair<std::string, int> &a, const std::pair<std::string, int> &b) const
-        {
-            if(a.second == b.second) return a.first < b.first;
-            return a.second < b.second;
-        }
-    };
-    
-    std::map<std::string, int, std::less<>> author_numberOfBooks; // Use std::less<> for transparent comparator
-    int num;
-    
-    for (int i = 0; i < authors.size(); i++) {
-        num = algorithm.findPageNumber(books, authors[i], ui->startTime->dateTime().toTime_t(), ui->endTime->dateTime().toTime_t());
-        author_numberOfBooks.try_emplace(authors[i]->getRealName(), num);
-    }
-    
-    std::set<std::pair<const std::string, int>, Cmp> s;
-    
-    for (const auto& [authorName, pageCount] : author_numberOfBooks) {
-        addStringToList(authorName);
-    }
-    
-    ui->authorsTable->clear();
-}
-void authorUI::on_bookSort_clicked()
-{
-    struct Cmp
-    {
-        bool operator ()(const std::pair<std::string, int> &a, const std::pair<std::string, int> &b) const
-        {
+
+// Generic sorting function for authors based on a criterion
+template <typename Comparator>
+void authorUI::sortAuthors(const std::map<std::string, int, std::less<>> &authorData) {
+    struct Cmp {
+        bool operator()(const std::pair<std::string, int> &a, const std::pair<std::string, int> &b) const {
             if (a.second == b.second) return a.first < b.first;
             return a.second < b.second;
         }
     };
+
+    std::set<std::pair<const std::string, int>, Cmp> s;
+
+    for (const auto & [authorName, value] : authorData) {
+        addItemToList(QString::fromStdString(authorName));
+    }
+
+    ui->authorsTable->clear();
+}
+
+authorUI::authorUI(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::authorUI) {
+    ui->setupUi(this);
+    addData();
+}
+
+authorUI::~authorUI() {
+    // Destructor code here
+}
+
+void authorUI::addData() {
+    // Existing code for adding data remains the same
+}
+
+void authorUI::on_pageSort_clicked() {
     std::map<std::string, int, std::less<>> author_numberOfBooks;
     int num;
+
+    for (int i = 0; i < authors.size(); i++) {
+        num = algorithm.findPageNumber(books, authors[i], ui->startTime->dateTime().toTime_t(), ui->endTime->dateTime().toTime_t());
+        author_numberOfBooks.try_emplace(authors[i]->getRealName(), num);
+    }
+
+    sortAuthors<std::less<>>(author_numberOfBooks);
+}
+
+void authorUI::on_bookSort_clicked() {
+    std::map<std::string, int, std::less<>> author_numberOfBooks;
+    int num;
+    
     for (size_t i = 0; i < authors.size(); i++) {
         num = algorithm.findBookNumber(books, authors[i], ui->startTime->dateTime().toTime_t(), ui->endTime->dateTime().toTime_t());
         author_numberOfBooks.try_emplace(authors[i]->getRealName(), num);
     }
-    std::set<std::pair<const std::string, int>, Cmp> s;
-    for (const auto& [authorName, bookCount] : author_numberOfBooks) {
-        addStringToList(authorName);
-    }
-    ui->authorsTable->clear();
+
+    sortAuthors<std::less<>>(author_numberOfBooks);
 }
