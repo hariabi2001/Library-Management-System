4b87d5d2bc4bf1cf044da9dcba5aacb6c2953f55
hariabi2001 <61871536+hariabi2001@users.noreply.github.com>
2023-10-12 22:47:53 +0530

    Update mainwindow.h

--- Book-library/mainwindow.h
+++ Book-library/mainwindow.h
@@ -14,7 +14,7 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    explicit MainWindow(QWidget *parent = nullptr);
 
 private slots:
     void on_Authors_clicked();
